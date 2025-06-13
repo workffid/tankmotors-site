396e28086ebebfcd627fbb8749b02abe24c3eb89
윤현동 <yhd5113@gmail.com>
2025-06-13 14:44:12 +0900

    프로젝트 초기 커밋

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+/* 터미널에서 탱크모터스로 이동 후 npm run dev 라고 치면 서버 실행 > 링크뜬곳으로 접속하면 구동완 */
